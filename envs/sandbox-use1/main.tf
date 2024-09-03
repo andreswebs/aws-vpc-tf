bbbe3cdb21c6dff05bf79e86d20da59e375a20b5
Andre Silva <andreswebs@pm.me>
2024-09-03 17:07:04 -0300

    input az_indexes

--- envs/sandbox-use1/main.tf
+++ envs/sandbox-use1/main.tf
@@ -4,5 +4,5 @@ module "network" {
   network_cidr_ipv4         = var.network_cidr_ipv4
   private_subnet_cidrs_ipv4 = var.private_subnet_cidrs_ipv4
   public_subnet_cidrs_ipv4  = var.public_subnet_cidrs_ipv4
-  az_items                  = "1,3,5"
+  az_indexes                = "2,4,6"
 }
