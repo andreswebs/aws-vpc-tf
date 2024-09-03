c80dc0e6fa9dc3764c751dad49f56cda39c39605
Andre Silva <andreswebs@pm.me>
2024-09-03 13:47:46 -0300

    azs

--- envs/sandbox-use1/main.tf
+++ envs/sandbox-use1/main.tf
@@ -4,4 +4,5 @@ module "network" {
   network_cidr_ipv4         = var.network_cidr_ipv4
   private_subnet_cidrs_ipv4 = var.private_subnet_cidrs_ipv4
   public_subnet_cidrs_ipv4  = var.public_subnet_cidrs_ipv4
+  az_items                  = "1,3,5"
 }
