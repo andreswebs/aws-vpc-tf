1b9d56ea714aa7ba0d9e7896ab19203e079c5da2
Andre Silva <andreswebs@pm.me>
2025-04-15 09:25:32 -0300

    upd

--- envs/sandbox-use1/main.tf
+++ envs/sandbox-use1/main.tf
@@ -1,8 +1,11 @@
 module "network" {
-  source                    = "../../modules/network"
-  network_name              = var.network_name
-  network_cidr_ipv4         = var.network_cidr_ipv4
-  private_subnet_cidrs_ipv4 = var.private_subnet_cidrs_ipv4
-  public_subnet_cidrs_ipv4  = var.public_subnet_cidrs_ipv4
-  az_indexes                = "2,4,6"
+  source     = "andreswebs/vpc/aws"
+  version    = "0.0.1"
+  name       = var.network_name
+  cidr_block = var.network_cidr_ipv4
+
+  private_subnet_cidr_blocks = var.private_subnet_cidrs_ipv4
+  public_subnet_cidr_blocks  = var.public_subnet_cidrs_ipv4
+
+  az_indexes = "2,4,6"
 }
