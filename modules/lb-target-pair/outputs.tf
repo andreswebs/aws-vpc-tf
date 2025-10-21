a88244b79600a1abfde777b65651418055992a2b
Andre Silva <andreswebs@pm.me>
2025-10-21 09:17:28 -0300

    rm unused
    Signed-off-by: Andre Silva <andreswebs@pm.me>

--- modules/lb-target-pair/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "target_group" {
-  description = "A list containing the pair of target group resources (`aws_lb_target_group`) used for blue-green deployment"
-  value       = aws_lb_target_group.this
-}
